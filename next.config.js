4962efd80b0bc39e4379394ed3c519056a0eda6d
Nsanda Antonio <drnsanda@gmail.com>
2023-10-13 10:48:16 +0100

    fix(build): inserted option to ignore eslint on builds

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ module.exports = {
   //     // a non-locale prefixed path e.g. `/hello`
   //     defaultLocale: 'en-US',
   //   },
-  ignoreDuringBuilds: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
 
 };
