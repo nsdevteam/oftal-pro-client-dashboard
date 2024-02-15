35ed581b38a9bcb2d4e05628eb80e4b58fe42080
git-marcopitra <marcomorete73@hotmail.com>
2024-02-15 10:17:29 +0100

    :green_heart: fix: export

--- hooks/index.tsx
+++ hooks/index.tsx
@@ -1,5 +1,4 @@
 export * from './use-click-outside-listener-ref';
 export { default as useEventListener } from './use-event-listener';
 export { default as useFirebase } from './use-firebase';
-export { default as useFormInput } from './use-form-input';
 export { default as useIsMounted } from './use-is-mounted';
