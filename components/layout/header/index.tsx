6ec8eccca14e7e7cfd9b9fb74c9968f2aa085654
git-marcopitra <marcomorete73@hotmail.com>
2024-02-19 12:46:45 +0100

    :fire: fix: remove badge

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -30,36 +30,6 @@ const Header: FC<HeaderProps> = ({ setOpenMenu }) => (
     >
       <LogoSVG width={200} height={30} />
     </Box>
-    <Box>
-      <Box
-        padding="0.5rem"
-        borderRadius={25}
-        display="flex"
-        nHover={{
-          background: '#E5E5E5',
-          transition: 'all 1s ease-out',
-        }}
-        justifyContent="center"
-      >
-        <Box
-          as="span"
-          position="absolute"
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          height={20}
-          width={20}
-          fontSize={12}
-          ml={15}
-          mt={-7}
-          borderRadius="100%"
-          bg="#f00"
-          color="#fff"
-        >
-          9
-        </Box>
-      </Box>
-    </Box>
   </Box>
 );
 
