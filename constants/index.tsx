9bac8916ddfa1ef4d8fa9ab82210387be01e4264
git-marcopitra <marcomorete73@hotmail.com>
2024-04-11 11:19:06 +0100

    :sparkles: feat: add order form to edit orders

---
+++ constants/index.tsx
@@ -0,0 +1,18 @@
+import { FiCreditCard, FiFileText } from 'react-icons/fi';
+
+import { MenuProps } from '../interface';
+
+export const menuLink: MenuProps[] = [
+  {
+    id: 1,
+    url: '/orders',
+    title: 'Pedido',
+    icon: <FiFileText size={18} color="#FFF" style={{ margin: '0.5rem' }} />,
+  },
+  {
+    id: 2,
+    url: '/account',
+    title: 'Contas',
+    icon: <FiCreditCard size={18} color="#FFF" style={{ margin: '0.5rem' }} />,
+  },
+];
